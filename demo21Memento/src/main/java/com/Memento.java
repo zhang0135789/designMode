f7e4652b1c3701b965e8a6dedebc46247eca4a12
zhang0135789 <zz634682577@163.com>
2024-05-03 21:35:48 +0800

    备忘录模式

---
+++ demo21Memento/src/main/java/com/Memento.java
@@ -0,0 +1,24 @@
+package com;
+
+/**
+ * @Author xupengboo
+ * @Date 2024/3/11 12:51
+ */
+// 备忘录 - 备忘录类
+public class Memento {
+
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+}
