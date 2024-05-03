6a09d626b5d64ea70e6f36e5dcd4349a06aafc65
zhang0135789 <zz634682577@163.com>
2024-05-03 15:30:17 +0800

    单例设计模式

---
+++ demo03Prototype/src/main/java/com/Singleton.java
@@ -0,0 +1,19 @@
+package com;
+
+public class Singleton {
+
+    private Singleton() {
+
+    }
+
+    private static class SingletonHolder {
+        public static final Singleton INSTANCE = new Singleton();
+    }
+
+    // 全局访问点，用于获取单例实例
+    public static Singleton getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+
+}
